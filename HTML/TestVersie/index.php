5788f64a41b532da8bc46bdefc1803d2fa5d8070
Koda <reconcatnl@gmail.com>
2021-09-21 00:33:25 +0200

    Basic design setup with preparation on endresult.

--- HTML/TestVersie/index.php
+++ HTML/TestVersie/index.php
@@ -1,1 +1,34 @@
+<!--
+    *Author:		Jordy van Venrooij
+    *Creation Date: 20-09-x21
+-->
 <?php
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<?php
+    include("templates/head.html");
+    //!!!!!!!!!!!!!!!DO NOT PLACE HTML CODE ABOVE THIS!!!!!!!!!!!!!!!!!!!!!!
+?>
+<body>
+<?php
+    // Banner Title
+    include("templates/header+navmenu.html");
+?>
+
+
+
+
+
+
+
+<?php
+    //!!!!!!!!!!!!!!!DO NOT PLACE ANYTHING BENEATH THIS!!!!!!!!!!!!!!!!!!!!!!
+    //Footer
+    include("templates/footer.html");
+?>
+</body>
+</html>
