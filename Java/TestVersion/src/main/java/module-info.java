441b0c4186b420ae8bb8a90c4aba458ba13e6c4d
Koda <78423525+Koda-The-Fox@users.noreply.github.com>
2021-10-01 13:51:14 +0200

    Android App Initialisatie
    Made the android application inside the git.

--- Java/TestVersion/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module groepc.application.application {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens groepc.application.application to javafx.fxml;
-    exports groepc.application.application;
-}
